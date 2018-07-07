b250b781175b7d8be6c1f6a1220f08a316ea3d19
DESKTOP-63O693G\atana <atanaspashov19@gmail.com>
2018-07-07 18:18:07 +0300

    First Commit.

---
+++ TriangleProblem/src/Line.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Line {
+	public int startX,endX,startY,endY;
+	private static final int RWIDTH = TriangleProblem.WIDTH, RHEIGHT = TriangleProblem.HEIGHT; // R was for relative in this class
+	// previous working - change RWIDTH and RHEIGHT with TriangleProblem.WIDTH and TriangleProblem.HEIGHT
+	// previously RWIDTH and RHEIGHT was = TriangleProblem.WIDTH-1 and TriangleProblem.HEIGHT-1
+	public static ArrayList<Line> lines = new ArrayList<>();
+	public Line(int startX, int startY, int endX, int endY) {
+		this.startX = startX;
+		this.startY = startY;
+		this.endX = endX;
+		this.endY = endY;
+	}
+
+	public static void CreateAllLines() {
+		int counter = 1;
+		while (counter!=TriangleProblem.WIDTH/*/2*/) {
+			for (int i = 0; i < RHEIGHT; i++){
+				for (int j = 0; j < RWIDTH; j++){
+					/*if (j<TriangleProblem.WIDTH-1)
+					lines.add(new Line(j, i, (j+1), i));
+					 */
+				
+					/*else */ if (j<RWIDTH-counter) {
+						lines.add(new Line(j,i,(j+counter), i)); // keep in mind that line 1, 2, 3, 4 is the
+						// same line as 3, 4, 1, 2
+					}
+					if (i<RHEIGHT-counter) {
+						lines.add(new Line(j,i,j,(i+counter)));
+					} // ???
+					
+				}
+			}
+		counter++;
+		}
+		for (int i = 0; i < RHEIGHT; i++){
+			for (int j = 0; j < RWIDTH; j++){
+				if (j%2!=0&&i%2==0) {
+					lines.add(new Line(j,i,j+1,i+1));
+				} // TODO finish for all lines
+				if(j%2==0&&i%2!=0) {
+					lines.add(new Line(j,i,j+1,i+1)); 
+				}
+				
+				if (/*i>0&&*/j%2!=0&&i%2==0) {
+					lines.add(new Line(j,i,j+1,i-1)); 
+				}
+				if (/*i>0&&*/j%2==0&&i%2!=0) 
+				{
+					lines.add(new Line(j,i,j+1,i-1)); 
+				}
+				
+			}
+		}
+	}
+
+	// ???? 
+	public static boolean Contains(Line line, int lineStart, int lineEnd) {
+		if ((line.startX==lineStart||line.endX==lineStart)&&(line.startY==lineEnd||line.endY==lineEnd)) {
+			return true;
+		}
+		if ((line.startY==lineStart||line.endY==lineStart)&&(line.startX==lineEnd||line.endX==lineEnd)) {
+			return true;
+		}
+		return false;
+	}
+
+	
+}
---
+++ TriangleProblem/src/TriangleProblem.java
@@ -0,0 +1,67 @@
+
+public class TriangleProblem {
+	public static final int HEIGHT = 14;
+	public static final int WIDTH = 16;
+	private static char[][] MakeTheCoordinateSystem() {
+		char[][] co = new char[HEIGHT][WIDTH];
+		for (int i = 0; i < HEIGHT; i ++)
+			for (int j = 0; j < WIDTH; j++){
+				co[i][j] = '.';
+			}
+		
+		return co;
+	}
+
+	private static long TriangleCount(char []oneD){
+		long ALLtriangleCount = 0;
+		int space = WIDTH - 1; // between two of the three dots (first and last)
+		for (int i = 0; i < oneD.length; i ++){
+			for (int j = 0; j < oneD.length; j++){
+				for (int l = 0; l < oneD.length; l++) {
+					if (i!=j&&i!=l&&j!=l) {
+						if(Math.abs(i-j)>=space||Math.abs(i-l)>=space||Math.abs(l-j)>=space) {
+							ALLtriangleCount++;
+						}
+					}
+				}
+			}
+			if (space ==0) {space = WIDTH-1;}
+			else{space--;}
+		}
+		// TODO make this ^ do the same but in the opposite direction	
+		return ALLtriangleCount;
+	}
+	
+	private static char[] ConvertTo1D(char co[][]) {
+		int c = 0;
+		char[] oneD = new char[HEIGHT*WIDTH];
+		for (int i = 0; i < HEIGHT; i ++){
+			for (int j = 0; j < WIDTH; j++){
+			oneD[c] = co[i][j];	
+			c++;	
+			}
+			}
+		return oneD; 
+	}
+	
+	public static void main (String args[]){
+		char co[][] = MakeTheCoordinateSystem();
+		for (int i = 0; i < HEIGHT; i ++){
+			for (int j = 0; j < WIDTH; j++){
+				System.out.print(co[i][j] + " ");
+			}
+		System.out.println();
+		}
+		char oneD[] = ConvertTo1D(co);
+		long countT = TriangleCount(oneD);
+		System.out.print(countT);
+		
+		Line.CreateAllLines();
+		System.out.println();
+		for (int i = 0; i < Line.lines.size(); i++) {
+			System.out.println(Line.lines.get(i).startX + "," + Line.lines.get(i).startY + ";" + Line.lines.get(i).endX + "," + Line.lines.get(i).endY);
+			System.out.print(i + "  ");
+		}
+		
+	}	
+}
